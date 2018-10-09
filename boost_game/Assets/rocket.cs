a87401bb0b93a86cddccc61ab4f72e00c951f859
unknown <jamesschlader@yahoo.com>
2018-10-09 12:50:32 -0600

    Got the rocket to thrust.

---
+++ boost_game/Assets/rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+       if (Input.GetKey(KeyCode.Space))
+        {
+            print("Trust!");
+            rigidBody.AddRelativeForce(Vector3.down);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            print("Going right");
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            print("Going left");
+        }
+
+    }
+}
